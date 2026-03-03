0117d2a4859f9e85e93fb4e036123ef9f20002e9
Adam Anderson <148352166+anderada@users.noreply.github.com>
2026-03-03 14:05:22 -0500

    basic movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -11,23 +11,42 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveLeftFoot();
-        moveRightFoot();
+        MoveLeftFoot();
+        MoveRightFoot();
+        MoveCapsule();
     }
 
-    void moveLeftFoot()
+    void MoveLeftFoot()
     {
         Vector3 raw_input = new Vector3(Input.GetAxis("LF_Hor"),0, Input.GetAxis("LF_Ver"));
-        LeftFoot.transform.localPosition += raw_input * Time.deltaTime;
-
-        if (Input.GetAxis("LF_Stomp") > 0.2)
+        Vector3 distanceToFoot = LeftFoot.transform.position + (raw_input * Time.deltaTime);
+        distanceToFoot = transform.position - distanceToFoot;
+        if(distanceToFoot.magnitude <= 1.5f){
+            LeftFoot.transform.localPosition += raw_input * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             LeftFootPos = LeftFoot.transform.position;
         }
     }
-    void moveRightFoot()
+    void MoveRightFoot()
     {
         Vector3 raw_input = new Vector3(Input.GetAxis("RF_Hor"),0, Input.GetAxis("RF_Ver"));
         RightFoot.transform.localPosition += raw_input * Time.deltaTime;
+        Vector3 distanceToFoot = RightFoot.transform.position + (raw_input * Time.deltaTime);
+        distanceToFoot = transform.position - distanceToFoot;
+        if (distanceToFoot.magnitude <= 1.5f)
+        {
+            RightFoot.transform.localPosition += raw_input * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            RightFootPos = RightFoot.transform.position;
+        }
+    }
+
+    void MoveCapsule()
+    {
+        transform.position = new Vector3((LeftFootPos.x + RightFootPos.x) / 2f, transform.position.y, (LeftFootPos.z + RightFootPos.z) / 2f);
     }
 }
