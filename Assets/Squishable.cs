0c865e3ed1865f40ceaa3a9fc88e1728510e0460
Adam Anderson <148352166+anderada@users.noreply.github.com>
2026-03-04 10:32:42 -0500

    squish particles

--- Assets/Squishable.cs
+++ Assets/Squishable.cs
@@ -4,12 +4,15 @@ public class Squishable : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    public ParticleSystem squishParticles;
+
     private void OnTriggerEnter(Collider other)
     {
         Foot foot = other.gameObject.GetComponent<Foot>();
         if (foot != null)
         {
             Destroy(this.gameObject);
+            squishParticles.Play();
         }
     }
 }
